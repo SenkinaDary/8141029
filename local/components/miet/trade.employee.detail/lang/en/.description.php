5cd312a4f4cb30900e35dcd3b19f1bc3de49425b
bitrix <bitrix@8141029.miet>
2016-12-12 02:36:05 +0300

    Employee view has been created

---
+++ local/components/miet/trade.employee.detail/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS['NAME_INTRANET'] = "Corporation portal";
+$MESS['NAME_KPI'] = "Trade";
+$MESS['NAME_COMPONENT'] = "Enter the values of the planned sales statistics";
+$MESS['DESCRIPTION_COMPONENT'] = "Form filling planned statistical values of sales staff";
